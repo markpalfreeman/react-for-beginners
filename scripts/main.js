643272a04d066e35cecb1365a6da3669fbbe90b5
Mark <markpalfreeman@gmail.com>
2016-03-17 15:20:04 -0700

    Add Router and History for clean URLs

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var StorePicker = require('./layout/store-picker');
 var App = require('./layout/app');
 var fourOhFour = require('./components/four-oh-four');
+var createBrowserHistory = require('history/lib/createBrowserHistory');
 
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path='/' component={StorePicker}/>
+    <Route path='store/:storeId' component={App}/>
+    <Route path='*' component={fourOhFour}/>
+  </Router>
+)
 
 // Render App to DOM
-ReactDOM.render(<App/>, document.getElementById('main'));
+ReactDOM.render(routes, document.getElementById('main'));
