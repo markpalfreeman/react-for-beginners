d9f68e77ffd0e5a9b58371b0f4c0a1b70605a027
Mark <markpalfreeman@gmail.com>
2016-03-18 11:46:07 -0700

    Import Helpers, add submit function and route change on StorePicker

--- scripts/layout/store-picker.js
+++ scripts/layout/store-picker.js
@@ -1,13 +1,27 @@
 var React = require('react');
+var helpers = require('../helpers');
+var ReactRouter = require('react-router');
+var History = ReactRouter.History;
 
 var StorePicker = React.createClass({
 
+  mixins: [History],
+
+  goToStore: function (event) {
+    var storeId = this.refs.storeId.value;
+
+    event.preventDefault();
+    // Transition from <Storepicker> to <App> using input value (store name)
+    this.history.pushState(null, '/store/' + storeId);
+
+  },
+
   render: function () {
     return (
-      <form className="store-selector">
+      <form className='store-selector' onSubmit={this.goToStore}>
         <h2>Please enter A Store</h2>
-        <input type="text" ref="storeId" required />
-        <input type="submit" />
+        <input type='text' ref='storeId' defaultValue={helpers.getFunName()} required />
+        <input type='submit' />
       </form>
     )
   }
